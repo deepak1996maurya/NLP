acd715c8b0f917ce855e42dba374a9e6966d3c85
Deepak Maurya <maurya1996deepak>
2020-02-06 21:12:06 +0530

    nlp start

--- nlp.py
+++ nlp.py
@@ -1,6 +1,6 @@
 import spacy
 nlp = spacy.load('en_core_web_sm')
-doc = nlp(u'deepak maurya is. a good boy. priya is not a bad girl')
+doc = nlp(u'deepak maurya is a good boy. he is an engineering student')
 print(doc)
 for token in doc:
     print(token.text, token.pos_ , token.dep_)
